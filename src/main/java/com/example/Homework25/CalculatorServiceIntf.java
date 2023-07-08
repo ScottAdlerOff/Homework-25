1be0aa959eee5ef3a87bbd9d2da7f306c38591dc
Scott Adler <enclave38@gmail.com>
2023-07-08 21:52:54 +0300

    Домашняя работа №25

---
+++ src/main/java/com/example/Homework25/CalculatorServiceIntf.java
@@ -0,0 +1,10 @@
+package com.example.Homework25;
+
+public interface CalculatorServiceIntf {
+    String greetings();
+
+    int plus(int a, int b);
+    int minus(int a, int b);
+    int multiply(int a, int b);
+    float divide(float a, float b);
+}
